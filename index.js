62bbb246ba4eb46d7d335e0f3f2923186f681767
Virendra <vsjethra13@gmail.com>
2023-07-13 03:12:36 -0400

    done

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 // Importing modules
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Importing schema
 const shortU = require('./models/shortUrl');
+const req = require('express/lib/request');
 
 // Initializing express
 const app = express();
 
 // Connecting to MongoDB
 // localhost:27017/urlShortener
-mongoose.connect(MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err));
