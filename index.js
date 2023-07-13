6de3ce7da20eb2cc50e3e75a44fdebfe85d24050
Virendra <vsjethra13@gmail.com>
2023-07-13 03:01:29 -0400

    done

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const app = express();
 
 // Connecting to MongoDB
 // localhost:27017/urlShortener
-// mongodb+srv://urlshrinker:tTGsJca4OOEse8Ii@urlshrinker.og4lrcz.mongodb.net/
-mongoose.connect('mongodb+srv://urlshrinker:tTGsJca4OOEse8Ii@urlshrinker.og4lrcz.mongodb.net/', {
+mongoose.connect(MONGO_URI, {
   useNewUrlParser: true, useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err));
